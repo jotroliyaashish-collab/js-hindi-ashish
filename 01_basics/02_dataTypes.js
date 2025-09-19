09a82ce21444958a5ac7f9bbe6219da94eef50c1
jotroliyaashish-collab <jotroliyaashish@gmail.com>
2025-09-19 07:50:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)   we are using nodejs,  not browser
+
+console.log(3
+    +
+    3) // code readability that should be high
+
+    console.log("Ashish")
+
+    let name = "Ashish"
+    let age = 19
+    let isLoggedIn = false
+    let accountState; undifined
+    let state = " " //null
+
+    // number -->  2 to power 53
+    //  bigint
+    // string => ""
+    // null => standalone value
+    // symbol => unique
+    // undifined =>
+
+    // object
+
+    console.log(typeof undifined); // undifined
+    console.log(typeof null);  // object
